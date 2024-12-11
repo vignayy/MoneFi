33b32b9a5c6c8c4583026f4cb356761ba465c18c
Vignay Bandari <114191883+vignayy@users.noreply.github.com>
2024-12-11 15:56:42 +0530

    Radar (#10)

--- finance-app/finance-app-user/src/main/java/com/finance/user/model/ProfileModel.java
+++ finance-app/finance-app-user/src/main/java/com/finance/user/model/ProfileModel.java
@@ -1,9 +1,6 @@
 package com.finance.user.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -23,5 +20,6 @@ public class ProfileModel {
     private String phone;
     private String address;
     private double incomeRange;
+    @Column(columnDefinition = "TEXT")
     private String profileImage;
 }
